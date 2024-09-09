7a1375c5ca2f145a680e5b3973bd760d40c1ac69
RobBaban12 <robertllyod.baban-23@cpu.edu.ph>
2024-09-09 17:39:33 +0800

    Your commit message

--- odd_num.py
+++ odd_num.py
@@ -1,9 +1,8 @@
-<<<<<<<<< Temporary merge branch 1
+
 odd = [1,3,5,7,11,13]
 
 print(odd)
-=========
 def odd(num):
     return [odd for odd in num if odd % 2 == 0]
 print(odd([2,3,4,5,6]))
->>>>>>>>> Temporary merge branch 2
+
